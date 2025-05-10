babfc5e2f35899cfb5958e0b7bfac7f710e8abdc
jps27cse <jackpritomsoren@hotmail.com>
2025-05-10 19:50:09 +0600

    docker network map

--- api/src/lib/server.js
+++ api/src/lib/server.js
@@ -5,6 +5,13 @@ export const startServer = () => {
   const httpServer = express();
   const port = config.port;
 
+  httpServer.get("/ping", (req, res) => {
+    console.log(`Ping route: ${req.url} ${Date.now()}`);
+    res.status(200).json({
+      message: "Ping:test successfull",
+    });
+  });
+
   try {
     httpServer.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
